3c759c8dd70840efa08ce87a903b8fa6f5465e37
Aline Korshunova <aline.korshunova00@gmail.com>
2026-06-06 23:59:30 +0700

    setup app nav graph

---
+++ app/src/main/java/com/example/bookflow/ui/navigation/AppNavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.bookflow.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.bookflow.ui.screens.details.BookDetailsScreen
+import com.example.bookflow.ui.screens.search.SearchScreen
+import com.example.bookflow.ui.screens.shelf.MyShelfScreen
+
+@Composable
+fun AppNavGraph(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = AppBottomDestination.Search.name,
+        modifier = modifier,
+    ) {
+        composable(route = AppBottomDestination.Search.name) {
+            SearchScreen()
+        }
+        composable(route = AppBottomDestination.BookDetails.name) {
+            BookDetailsScreen()
+        }
+        composable(route = AppBottomDestination.MyShelf.name) {
+            MyShelfScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bookflow/ui/screens/details/BookDetailsScreen.kt
@@ -0,0 +1,32 @@
+package com.example.bookflow.ui.screens.details
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookflow.ui.theme.BookFlowTheme
+
+@Composable
+fun BookDetailsScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+    ) {
+        Text("BookDetails")
+    }
+}
+
+@Preview(name = "Light Theme", showBackground = true)
+@Preview(name = "Dark Theme", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun BookDetailsScreenPreview() {
+    BookFlowTheme {
+        BookDetailsScreen()
+    }
+}
