100b007e5b1846499ac343d7c0f228f509eb201e
Aline Korshunova <aline.korshunova00@gmail.com>
2026-06-15 15:43:06 +0700

    add paging ui components

---
+++ app/src/main/java/com/example/bookflow/ui/components/paging/PagingLoadingItem.kt
@@ -0,0 +1,39 @@
+package com.example.bookflow.ui.components.paging
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bookflow.ui.theme.BookFlowTheme
+
+@Composable
+fun PagingLoadingItem(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 24.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(name = "Light Theme", showBackground = true)
+@Preview(name = "Dark Theme", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PagingLoadingItemPreview() {
+    BookFlowTheme {
+        Surface {
+            PagingLoadingItem()
+        }
+    }
+}
